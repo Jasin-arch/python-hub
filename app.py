f5c4c4da46058c2373a9359c809c907fd8aeb62d
jasin-arch <fredrickjasin7@gmail.com>
2026-02-18 08:14:46 +0300

    updated python1.1

---
+++ app.py
@@ -0,0 +1,73 @@
+# this is a single line comment
+name = "John Doe" # this is a variable
+# firstName = input()
+# secondname = input("Enter second name:")
+secondName = "Fredrick Jasin"
+
+"""
+Multi-line comment or string
+"""
+
+
+
+
+
+
+
+"""
+Multi-line comment or string
+"""
+
+"""
+DATA TYPE:
+   -TEXT(strings) type: str
+   -NUMBERS(integers, float, complex)
+   -LIST(list, tuple, dictionary, sets)
+"""
+# Numbers: INT ->int()
+number = 10
+output = number
+output = type(number)
+number = 1000000000
+output = number
+numberTwo = 20000
+output = numberTwo
+
+# Number: FLOAT ->float()
+
+amount = 20.5
+output = amount
+output = type(amount)
+
+# Number: COMPLEX ->complex()
+root = -2j
+output = root
+output = type(root)
+
+# text: STRING -> str()
+name = "John Doe"
+name = "Fredrick Jasin"
+welcomeMessage = 'It\'s a sunny morning'
+welcomeMessage = "It's a sunny morning"
+
+
+output = welcomeMessage
+output = type(welcomeMessage)
+
+
+
+
+
+
+
+
+
+
+print("====================")
+print(output)
+print("====="
+"===============")
+
+
+
+
