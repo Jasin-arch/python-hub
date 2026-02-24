36b1d6c2473d829d0654b02c2721f0fcd1632ee3
Hood-ley <hoodleymartin.lr@gmail.com>
2026-02-24 11:09:04 +0300

    new  python

--- oop.py
+++ oop.py
@@ -61,9 +61,80 @@ class Monitor:
 hpMonitor = Monitor("rectanular", "1080", "21", "2000", "gray")
 dellMonitor = Monitor(shape="oval", resolution="1280", size="36", yom="2010", color="black")
 
-hpMonitor.switchOnMonitor()
-dellMonitor.switchOnMonitor()
+# hpMonitor.switchOnMonitor()
+# dellMonitor.switchOnMonitor()
 
 
-print(f"===> {hpMonitor.shape}")
-print(f"===> {dellMonitor.shape}")
+# print(f"===> {hpMonitor.shape}")
+# print(f"===> {dellMonitor.shape}")
+
+
+
+class Car:
+
+    def __init__(self, model, color, isElectric):
+        self.model = model
+        self.color = color
+        self.isElectric = isElectric
+
+    def carFeatures(self):
+        print(f"THis vehecle is of model {self.model} it of color{self.color} and is electric {self.isElectric}")
+        
+    def startCar(self):
+        print(f"Starting oyur {self.model} car!")    
+
+tesla = Car("Tesla", "Space gray", True)
+toyota = Car("Toyota", "White", False)
+
+# tesla.carFeatures()
+# toyota.carFeatures()
+
+
+# # print(tesla.model)
+# # print(toyota.isElectric)
+# # print(f"THis vehecle is of model {self.model}")
+
+
+# class Tuktuk:
+
+#     def __init__(self, model, color, isElectric, numberOfWheels):
+#         super().__init__(model, color, isElectric)
+#         self.numberOfWheels = numberOfWheels
+
+#     def wheelNumber(self):
+#         print(f"Your tuktuk has {self.numberOfWheels}")   
+
+    
+
+
+
+class Book:
+
+    def __init__(self, title, author, yop, publisher, costOfProduction):
+        self.title = title
+        self.author = author
+        self.yop = yop
+        self.publisher = publisher
+        self.costOfProduction = costOfProduction
+
+
+    def aboutBook(self):
+        print(f"TITLE: {self.title}")
+        print(f"AOUTHOR: {self.author}")
+        print(f"YOP: {self.yop}")
+        print(f"PUBLISHER: {self.publisher}")
+        print(f"PRODUCTION: {self.costOfProduction}")
+
+# alchemist = Book()
+# word = Book()
+
+alchemist = Book("The Alchemist", "Paulo Jasin", "1988", "Kasuku", "3500")
+word = Book("The Word", "Fredrick", "2026", "crown", "2000")
+
+output = alchemist.author
+alchemist.aboutBook()
+word.aboutBook()
+
+# print(output)
+
+        
